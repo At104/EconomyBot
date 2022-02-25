19659386b78a78e83043d9797a7f2b5a50149da6
Atul R | SomethingClever <atullego2014@gmail.com>
2022-02-25 17:27:14 -0500

    "Updating" send function

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import time
-#client = commands.Bot(command_prefix = '!', description="") # Defines the command prefix to be "!"
+#bot = commands.Bot(command_prefix = '!', description="") # Defines the command prefix to be "!"
 bot = discord.Bot()
 
 load_dotenv()
@@ -19,23 +19,47 @@ async def on_ready():
 testingservers = [783415814342574151]
 
 @bot.slash_command(guild_ids = testingservers, name = "hello", description = "Just a command to test slash commands!")
-async def hello(ctx, name: str = None):
-    name = name or ctx.author.name
+async def hello(ctx, nameinput: str = ""):
+    name = nameinput or ctx.author.nameinput
     await ctx.respond(f"Hello {name}!")
     
-@bot.slash_command()
-async def send(ctx):
-    await ctx.send("How much do you want to send?")  
+@bot.slash_command(name = "send", description = "Send another person money")
+async def send(ctx, nameinput = "", amount = ""):
     
-    try:
-        message = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=15.0)
-    except asyncio.TimeoutError:
-        await ctx.send("timeout stoopid")
-    else:
-        if message.content.lower() == " ":
-            await ctx.send("Your entered value is blank!")
+    if (amount == "" and nameinput == ""):
+        await ctx.respond("Need to input at least the reciever or amount you want to send!")
+        
+    elif (amount == ""):
+        await ctx.respond("How much do you want to send?") 
+        try:
+            amount = await bot.wait_for("amount", check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=15.0)
+        except asyncio.TimeoutError:
+            await ctx.respond("timeout stoopid")
         else:
-            await ctx.send("Sent " + message.content.lower() + " !")
+            if amount.content.lower() == " ":
+                await ctx.respond("Your entered value is blank!")    
+            else:
+                await ctx.respond("Sent " + amount + " to " + nameinput + "!")
+                     
+    elif (nameinput == ""):
+        await ctx.respond("Who do you want to send the money to?") 
+        try:
+            nameinput = await bot.wait_for("nameinput", check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=15.0)
+        except asyncio.TimeoutError:
+            await ctx.respond("timeout stoopid")
+        else:
+            if nameinput == " ":
+                await ctx.respond("Your entered value is blank!")
+            else:
+                await ctx.respond("Sent " + amount + " to " + nameinput + "!")
+    
+
+    
+    else:
+        await ctx.respond("Sent " + amount + " to " + nameinput + "!")
+   
+        
+   
     
     
 bot.run(os.getenv("apikey"))
