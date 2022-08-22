a27e241b56555e0f22514203b700e2653c1da976
Minh Nguyen <77513059+Raminh05@users.noreply.github.com>
2022-08-22 18:53:07 -0400

    Update function documentation

--- utils/idcheck.py
+++ utils/idcheck.py
@@ -1,6 +1,6 @@
 def id_check(id_nums: dict, ids: list) -> dict:
 
-    """ Check if Discord user id is in the json database """
+    """ Check if Discord user id(s) are in the json database """
     for id in ids:
         if str(id) not in id_nums:
             id_nums[str(id)] = [{"id": id,"amount": 0}]
