715893dfe899e1e22c2af9ec9962e832ec9e9685
Atul R | SomethingClever <83225748+At104@users.noreply.github.com>
2025-01-20 16:37:48 -0500

    Clean filtermessage

--- cogs/filtermessage.py
+++ cogs/filtermessage.py
@@ -23,17 +23,7 @@ class FilterMessage(commands.Cog):
         role = guild.get_role(role_id)
         
         msg_content = message.content.lower().replace(" ", "")
-        pattern = re.compile(r'p+o+k+e*r*|p+0+k+e*r*|p+o+k+3*r*|p+0+k+3*r*')
-        pattern2 = re.compile(r'p+e+e*|p+3+e*|p+e+3*|p+3+e*|p+o+o*|p+0+o*|p+o+0*|p+0+0*|p+e+p+e*|p+o+p+o*')
-        
-        if message.author.id == certain_id and pattern.search(msg_content):
-            await message.delete()
-            await message.channel.send("no poker for you")
-        
-        elif message.author.id == certain_id and pattern2.search(msg_content):
-            await message.delete()
-            await message.channel.send("no just stop")
-            
+       
         if role in message.author.roles:     
             print("Checking words")  
             words = self.scan_for_words(msg_content, ["sell", "offer", "ticket", "coldplay", "bus", "pass", "concert", "concerts", "selling", "buy", "prep101", "hsr", "tutor", "taylor swift"])
